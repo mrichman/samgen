73f3045e75725a66b67b94fc4772ca5ab40b7fea
Mark Richman <mark@markrichman.com>
2021-07-02 21:50:52 -0400

    Added API events

--- util/resources.go
+++ util/resources.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/awslabs/goformation/v5/cloudformation/serverless"
@@ -8,8 +9,29 @@ import (
 )
 
 func GenerateServerlessFunction(verb string, operation *openapi3.Operation) (*serverless.Function, error) {
+
+	events := map[string]serverless.Function_EventSource{
+		strings.ToTitle(verb) + "Resource": {
+			Type: "Api",
+			Properties: &serverless.Function_Properties{
+				ApiEvent: &serverless.Function_ApiEvent{
+					Path:   "/" + operation.OperationID,
+					Method: strings.ToLower(verb),
+				},
+			},
+		},
+	}
+
+	functionPath := fmt.Sprintf("functions/%s", operation.OperationID)
 	function := &serverless.Function{
-		Description: operation.Description, FunctionName: strings.Title(operation.OperationID),
+		Description:  operation.Description,
+		FunctionName: strings.Title(operation.OperationID),
+		Handler:      "app.lambda_handler",
+		CodeUri: &serverless.Function_CodeUri{
+			String: &functionPath,
+		},
+		Runtime: "python3.9",
+		Events:  events,
 	}
 	return function, nil
 }
